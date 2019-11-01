0d38de761f7c56bc547f702e167dd372433b5884
Nathan Embery <nembery@juniper.net>
2019-11-01 12:31:10 -0400

    Create score_all_skillets.py

---
+++ score_all_skillets.py
@@ -0,0 +1,38 @@
+import os
+
+from skilletlib.skilletLoader import SkilletLoader
+
+output_format = os.environ.get('skillet_output', 'table')
+directory = os.environ.get('skillet_directory', '~/.pan_cnc/panhandler/repositories/')
+sl = SkilletLoader()
+
+if '~' in directory:
+    d = os.path.expanduser('~/.pan_cnc/panhandler/repositories/')
+else:
+    d = directory
+
+skillets = sl.load_all_skillets_from_dir(d)
+
+if output_format == 'table':
+    print('_' * 145)
+    print(f'|{"Skillet":^90}|{"Type":^15}|{"Variables":^10}|{"Snippets":^10}|{"Lines of Config":^15}|')
+    print('_' * 145)
+else:
+    print('Skillet,Type,Variables,Snippets,Lines of Config')
+
+for s in skillets:
+    num_vars = len(s.variables)
+    num_snippets = len(s.snippet_stack)
+    num_xml_lines = 0
+    if 'pan' in s.type:
+        for snippet in s.get_snippets():
+            if 'element' in snippet.metadata:
+                num_xml_lines += len(snippet.metadata['element'].split('\n'))
+
+    if output_format == 'table':
+        print(f'|{s.label:90}|{s.type:15}|{num_vars:10}|{num_snippets:10}|{num_xml_lines:15}|')
+    else:
+        print(f'{s.name},{s.type},{num_vars},{num_snippets},{num_xml_lines}')
+
+if output_format == 'table':
+    print('-' * 145)
